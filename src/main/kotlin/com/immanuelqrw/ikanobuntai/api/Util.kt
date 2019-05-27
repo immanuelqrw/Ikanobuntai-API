3896e11a78d34f6b5f3eb48b8c0291ad98ba68e7
Immanuel Washington <code.immanuelqrw@gmail.com>
2019-05-27 17:19:45 -0400

    Added setOfNotNull
    Helper method for use later in types

---
+++ src/main/kotlin/com/immanuelqrw/ikanobuntai/api/Util.kt
@@ -0,0 +1,12 @@
+package com.immanuelqrw.ikanobuntai.api
+
+
+/**
+ * Returns a new read-only set either of single given element, if it is not null, or empty set if the element is null. The returned set is serializable (JVM).
+ */
+public fun <T : Any> setOfNotNull(element: T?): Set<T> = if (element != null) setOf(element) else emptySet()
+
+/**
+ * Returns a new read-only set only of those given elements, that are not null.  The returned set is serializable (JVM).
+ */
+public fun <T : Any> setOfNotNull(vararg elements: T?): Set<T> = elements.filterNotNull().toSet()
